5e76b10c7564f458ff6dff3c03e4be3cd2ee7e08
Begzod Muxamedov <bmuxamedov585@bk.ru>
2019-10-03 14:56:21 +0500

    feat: db updater

--- src/helper.js
+++ src/helper.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
+const dbAdress = 'database.json';
+if (!(fs.existsSync(dbAdress))) {
+    fs.writeFileSync(dbAdress, JSON.stringify({}));
+}
 
 module.exports = {
     logStart() {
         console.log('Bot has been Started ...');
     },
-    checkUser(id) {
-        return fs.existsSync(`./users/${id}.json`);
+    
+    updateDB(id, newUsers) {
+        const db = JSON.parse(fs.readFileSync(dbAdress, 'utf-8'));
+        if (db[id] == undefined) {
+            db[id] = newUsers.map((el) => el.id);
+        }
+        else if
+        const newUsers_IDs = newUsers.map((el) => el.id);
+        db[id] = db[id] ? db[id].concat(newUsers_IDs) : newUsers_IDs;
+        fs.writeFileSync(dbAdress, JSON.stringify(db));
     }
+    
 }
--- src/index.js
+++ src/index.js
@@ -8,32 +8,17 @@ const bot = new TelegramBot(config.TOKEN, {
 });
 _.logStart();
 
-let db = JSON.parse(fs.readFileSync('database.json', 'utf-8'));
-db[0]['482944903'] = db[0]['482944903'] ? db[0]['482944903'].concat([123]) : [123];
-
-function readDB(path) {
-    return JSON.parse(fs.readFileSync(path, 'utf-8'));
-}
-function updateDB(db, id, newUsers) {
-    const newUsers_IDs = newUsers.map((el) => el.id)
-    db[id] = db[id] ? db[id].concat(newUsers_IDs) : newUsers_IDs;
-    fs.writeFile('database.json', JSON.stringify(db), (err) => {console.log(err)});
-    return db;
-}
-
-fs.writeFileSync('database.json', JSON.stringify(db));
-
 bot.on('message', msg => {
     console.log('New Message ', msg);
 
     if (msg.new_chat_members) {
-        
+        _.updateDB(msg.from.id, msg.new_chat_members);
         // bot.sendMessage(msg.chat.id, `New Member ${msg.new_chat_members[0].first_name}`);
-        bot.deleteMessage(msg.chat.id, msg.message_id);
+        // bot.deleteMessage(msg.chat.id, msg.message_id);
     } else if (msg.left_chat_member) {
         // console.log('New Message ', msg);
         // bot.sendMessage(msg.chat.id, `Left Member ${msg.left_chat_member.first_name}`);
-        bot.deleteMessage(msg.chat.id, msg.message_id);
+        // bot.deleteMessage(msg.chat.id, msg.message_id);
     }
 });
 
